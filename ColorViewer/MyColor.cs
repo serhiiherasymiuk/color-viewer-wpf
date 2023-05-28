c57e4b220eec3e3680293132342189ecee3c6583
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-05-28 22:13:54 +0300

    Add project files.

---
+++ ColorViewer/MyColor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColorViewer
+{
+    internal class MyColor : INotifyPropertyChanged
+    {
+        private byte a;
+        public byte A
+        {
+            get => a;
+            set
+            { 
+                a = value;
+                OnPropertyChanged(nameof(A));
+                OnPropertyChanged(nameof(ColorInfo));
+            }
+        }
+        private byte r;
+        public byte R
+        {
+            get => r;
+            set
+            {
+                r = value;
+                OnPropertyChanged(nameof(R));
+                OnPropertyChanged(nameof(ColorInfo));
+            }
+        }
+        private byte g;
+        public byte G
+        {
+            get => g;
+            set
+            {
+                g = value;
+                OnPropertyChanged(nameof(G));
+                OnPropertyChanged(nameof(ColorInfo));
+            }
+        }
+        private byte b;
+        public byte B
+        {
+            get => b;
+            set
+            {
+                b = value;
+                OnPropertyChanged(nameof(B));
+                OnPropertyChanged(nameof(ColorInfo));
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Color ColorInfo => new Color() { A = A, R = R, G = G, B = B };
+        public MyColor Clone()
+        {
+            MyColor color = (MyColor)this.MemberwiseClone();
+            color.A = (byte)this.A;
+            color.R = (byte)this.R;
+            color.G = (byte)this.G;
+            color.B = (byte)this.B;
+            return color;
+        }
+    }
+}
---
+++ ColorViewer/ViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ColorViewer
+{
+    internal class ViewModel
+    {
+        private RelayCommand addCommand;
+        private RelayCommand removeCommand;
+        private RelayCommand clearCommand;
+        private ObservableCollection<MyColor> colors = new ObservableCollection<MyColor>();
+        public IEnumerable<MyColor> Colors => colors;
+        public MyColor CurrentColor { get; set; }
+        public MyColor SelectedColor { get; set; }
+        public ViewModel()
+        {
+            addCommand = new RelayCommand((o) => AddColor(),(obj) => checkAddButton() != false);
+            removeCommand = new RelayCommand((o) => RemoveColor(), (o) => SelectedColor != null);
+            clearCommand = new RelayCommand((o) => colors.Clear(), (o) => colors.Any());
+
+            CurrentColor = new MyColor() {A = 255 };
+        }
+        public ICommand AddCommand => addCommand;
+        public ICommand RemoveCommand => removeCommand;
+        public ICommand ClearCommand => clearCommand;
+        public void RemoveColor()
+        {
+            if (CurrentColor != null)
+                colors.Remove(SelectedColor);
+        }
+        public void AddColor()
+        {
+            bool flag = true;
+            foreach (MyColor color in colors)
+            {
+                if(CurrentColor.ColorInfo == color.ColorInfo)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+            {
+                colors.Add(CurrentColor.Clone());
+            }
+        }
+        public void Clear()
+        {
+            if (colors.Any())
+                colors.Clear();
+        }
+
+        public bool checkAddButton()
+        {
+            bool flag = true;
+            foreach (MyColor color in colors)
+            {
+                if (CurrentColor.ColorInfo == color.ColorInfo)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+}
